b3858cd463cb2678be1a3ea5a33b2e83796ac7ae
Mateusz Chmielewski <hello@itsmattch.com>
2025-06-04 22:39:26 +0200

    Simplify abstraction resolution

--- config/gildsmith.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'models' => [
-        \Gildsmith\Contract\Product\Attribute::class => '',
-        \Gildsmith\Contract\Product\AttributeValue::class => '',
-    ],
-];
--- src/Model/Concerns/HasAbstractRelationships.php
+++ src/Model/Concerns/HasAbstractRelationships.php
@@ -2,6 +2,7 @@
 
 namespace Gildsmith\Support\Model\Concerns;
 
+use Gildsmith\Support\Utils\ResolvesInstance;
 use Illuminate\Database\Eloquent\Concerns\HasRelationships;
 use Illuminate\Database\Eloquent\Model;
 
@@ -16,25 +17,10 @@ trait HasAbstractRelationships
 
     protected function newRelatedInstance($class)
     {
-        return tap($this->resolveInstance($class), function ($instance) {
+        return tap(resolve($class), function ($instance) {
             if (! $instance->getConnectionName()) {
                 $instance->setConnection($this->connection);
             }
         });
-    }
-
-    protected function resolveInstance(string $class)
-    {
-        $instance = config("gildsmith.models.$class");
-
-        if (is_string($instance)) {
-            return new $instance;
-        }
-
-        if (app()->bound($class)) {
-            return app($class);
-        }
-
-        return new $class;
     }
 }
